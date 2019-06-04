d72f0dec6035e53775755c60e6f7bf6f44b3ebff
zwb <zhuangweibin@stbl.cc>
2019-06-04 17:37:12 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zwb_crawler",
+    version="1.0.2",
+    keywords=("util"),
+    description="zwb_crawler",
+    long_description="zwb_crawler",
+    url="https://github.com/weibin268/zwb_crawler",
+    author="zhuang weibin",
+    author_email="448075543@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
